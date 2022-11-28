5398fa24d1e5ee6309f6b90fd2cf4a22f9ddbaa3
hasegawa2073 <hasegawatatsuya206@gmail.com>
2022-11-28 17:04:51 +0900

    ファイルに文字を書き込んだ

--- 02_node_basic/030_write_file/start/example.js
+++ 02_node_basic/030_write_file/start/example.js
@@ -1,1 +1,9 @@
-console.log('hello, node.js');
+// ファイルへの書き込み
+// __dirname, __filename
+
+const fs = require("fs");
+
+console.log(__filename);
+
+fs.writeFileSync(__dirname + "/test.txt", "hello, node.js");
+// console.log("hello, node.js");
