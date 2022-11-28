4962fed33d0c6f7cd6f8423e09e75b77fe4af14f
hasegawa2073 <hasegawatatsuya206@gmail.com>
2022-11-28 17:12:10 +0900

    パスの取り扱い方法について学んだ。path.resolve

--- 02_node_basic/030_write_file/start/example.js
+++ 02_node_basic/030_write_file/start/example.js
@@ -2,8 +2,11 @@
 // __dirname, __filename
 
 const fs = require("fs");
+const path = require("path");
 
-console.log(__filename);
+const distPath = path.resolve(__dirname, "../dist/test.txt");
 
-fs.writeFileSync(__dirname + "/test.txt", "hello, node.js");
+console.log(distPath);
+
+fs.writeFileSync(distPath, "hello, node.js");
 // console.log("hello, node.js");
